a91c121a59d408d83f1d5daa5546affb6b66b0d9
Moch Aldrian <mhmdsaldrian96@gmail.com>
2023-08-18 17:15:20 +0700

    First Commit

---
+++ src/views/KingView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="king-wrapper">
+    <h1>Hello</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
